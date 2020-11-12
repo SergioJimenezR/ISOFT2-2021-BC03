b41b586f999163bf9e4d0d0b1485585ce5bb4ff6
JoseMiguelMartinezMalonda <72039571+JoseMiguelMartinezMalonda@users.noreply.github.com>
2020-11-12 17:35:56 +0100

    Interfaz camarero de mesa

---
+++ BC03/PedidosComandas/src/main/java/es/uclm/esi/isoft2/PedidosComandas/Presentacion/IU_Camarero.java
@@ -0,0 +1,42 @@
+package es.uclm.esi.isoft2.PedidosComandas.Presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class IU_Camarero extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IU_Camarero frame = new IU_Camarero();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IU_Camarero() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
