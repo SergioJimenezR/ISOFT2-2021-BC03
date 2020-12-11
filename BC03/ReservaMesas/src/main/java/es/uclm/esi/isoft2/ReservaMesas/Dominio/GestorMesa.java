f36d8b624ea2eb2ca72804f3439270870bbb4e55
DavidGB18 <71131821+DavidGB18@users.noreply.github.com>
2020-12-11 11:52:12 +0100

    cambio en ocupar mesa

--- BC03/ReservaMesas/src/main/java/es/uclm/esi/isoft2/ReservaMesas/Dominio/GestorMesa.java
+++ BC03/ReservaMesas/src/main/java/es/uclm/esi/isoft2/ReservaMesas/Dominio/GestorMesa.java
@@ -5,12 +5,13 @@ import javax.swing.JComboBox;
 
 import es.uclm.esi.isoft2.PedidosComandas.Dominio.*;
 public class GestorMesa {
-	public static boolean cambiarEstadoOcupado(int idMesa) {
+	public static boolean cambiarEstadoOcupado(int idMesa, String nombreCliente) {
 		JComboBox<Mesa> mesas = IU_CamareroMesa.getComboBoxMesas();
 		for (int i = 0; i < mesas.getItemCount(); i++) {
 			mesas.setSelectedIndex(i);
 			Mesa mesa = (Mesa) mesas.getSelectedItem();
-			if (mesa.getId() == idMesa) {
+			Cliente cliente = mesa.getCliente();
+			if (mesa.getId() == idMesa && nombreCliente.equals(cliente.getNombre())) {
 				mesa.setEstadoMesa(EstadosMesas.OCUPADA);
 				mesas.setSelectedItem(mesa);
 				return true;
