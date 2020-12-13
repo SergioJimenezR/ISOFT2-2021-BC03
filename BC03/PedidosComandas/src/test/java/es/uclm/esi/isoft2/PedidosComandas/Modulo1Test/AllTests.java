fdf0cd57683d876f424fdc906f5735a701ec2df1
SERGIO MOLINA LÓPEZ <Sergio.Molina10@alu.uclm.es>
2020-12-13 10:19:18 +0100

    Ejecucicón de todas las clases de pruebas modulo 1

---
+++ BC03/PedidosComandas/src/test/java/es/uclm/esi/isoft2/PedidosComandas/Modulo1Test/AllTests.java
@@ -0,0 +1,12 @@
+package es.uclm.esi.isoft2.PedidosComandas.Modulo1Test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({  AppTest.class,AlmacenTest.class, BebidaTest.class, ComandaTest.class, MesaTest.class,
+		PlatoTest.class })
+public class AllTests {
+
+}
