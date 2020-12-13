2e7ab0e65e85ee4ba992c8a98e693c78635fcba5
SERGIO MOLINA LÓPEZ <Sergio.Molina10@alu.uclm.es>
2020-12-13 23:41:02 +0100

    Terminadas pruebas modulo 1

--- BC03/PedidosComandas/src/test/java/es/uclm/esi/isoft2/PedidosComandas/Modulo1Test/AllTests.java
+++ BC03/PedidosComandas/src/test/java/es/uclm/esi/isoft2/PedidosComandas/Modulo1Test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({  AlmacenTest.class, BebidaTest.class, ComandaTest.class, MesaTest.class,
+@SuiteClasses({AlmacenTest.class, BebidaTest.class, ComandaTest.class, MesaTest.class,
 		PlatoTest.class })
 public class AllTests {
 
