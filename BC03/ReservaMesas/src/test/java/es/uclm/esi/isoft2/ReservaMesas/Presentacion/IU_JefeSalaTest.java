884a9ad446331f8407128898cd23477f623e97bb
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2020-12-15 10:38:46 +0100

    testing IU_JefeSala

---
+++ BC03/ReservaMesas/src/test/java/es/uclm/esi/isoft2/ReservaMesas/Presentacion/IU_JefeSalaTest.java
@@ -0,0 +1,28 @@
+package es.uclm.esi.isoft2.ReservaMesas.Presentacion;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.swing.JFrame;
+
+import org.junit.Test;
+
+public class IU_JefeSalaTest {
+
+	@Test
+	public void testIU_JefeSala() throws Exception {
+		JFrame window = new IU_JefeSala();
+	}
+
+	@Test
+	public void testGetComboBoxReservadas() throws Exception {
+		JFrame window = new IU_JefeSala();
+		((IU_JefeSala) window).getComboBoxReservadas();
+	}
+
+	@Test
+	public void testSetComboBoxReservadas() throws Exception {
+		JFrame window = new IU_JefeSala();
+		((IU_JefeSala) window).setComboBoxReservadas(null);
+	}
+
+}
